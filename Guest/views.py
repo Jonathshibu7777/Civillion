a69676f8aeff59437f23596adddfd41e6953f95d
Jonath Shibu Pullorkudyil <jonathshibu777@gmail.com>
2024-02-26 09:29:46 +0530

    25-2-24

--- Guest/views.py
+++ Guest/views.py
@@ -8,12 +8,12 @@ def userRegistration(request):
     if request.method=="POST":
         place = tbl_Place.objects.get(id=request.POST.get('sel_place'))
         tbl_user.objects.create(user_name=request.POST.get("txtname"),user_gender=request.POST.get("gender"),user_contact=request.POST.get("txtcontact"),user_email=request.POST.get("txtemail"),user_photo=request.FILES.get("fileImage"),user_proof=request.FILES.get("fileProof"),user_password=request.POST.get("txtpwd"),place=place)
-        return redirect("Guest:userRegistration")
+        return redirect("guest:userRegistration")
     else:
-        return render(request,"Guest/NewUser.html",{"districtdata":district})
+        return render(request,"guest/NewUser.html",{"districtdata":district})
 
 def ajaxplace(request):
     dis = tbl_District.objects.get(id=request.GET.get("did"))
     place = tbl_Place.objects.filter(District_Name=dis)
-    return render(request,"Guest/AjaxPlace.html",{"placedata":place})
+    return render(request,"guest/AjaxPlace.html",{"placedata":place})
 
